5c5e919f7a9d43587161c85642b5da3f3894261c
Daniel Silva <danielviana212@gmail.com>
2026-07-11 15:46:28 -0300

    Adicionar arquivos de projeto.

---
+++ BarbeariaApi/Controllers/SaudeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarbeariaApi.Controllers;
+
+[ApiController]
+[Route("api/saude")]
+public class SaudeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Consultar()
+    {
+        return Ok(new
+        {
+            status = "Saudavel",
+            dataHora = DateTime.UtcNow,
+            aplicacao = "Barbearia API"
+        });
+    }
+}
---
+++ BarbeariaApi/Program.cs
@@ -0,0 +1,31 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Barbearia Daniel API")
+            .WithTheme(ScalarTheme.Purple)
+            .WithDefaultHttpClient(
+                ScalarTarget.CSharp,
+                ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
